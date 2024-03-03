a7dd9a32ce06916adc53f3090db8aba20d7bc04d
mohamedsabri98 <muhammed77171@gmail.com>
2024-03-03 14:32:24 +0200

    TODO

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import { useState, useRef } from "react";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [todos, setTodos] = useState([])
+
+    const inputRef = useRef()
+
+    const handleAddTodo = () => {
+        const txt = inputRef.current.value
+        const newItem = { completed: false, txt }
+        setTodos([...todos, newItem])
+        inputRef.current.value = ""
+    }
+
+    const handleItemDone = (index) => {
+        const newTodo = [...todos]
+        newTodo[index].completed = !newTodo[index].completed
+        setTodos(newTodo)
+    }
+
+    const handeDeleteItem = (index) => {
+        const newTodo = [...todos]
+        newTodo.splice(index, 1)
+        setTodos(newTodo)
+    }
+
+    return (
+        <div className="App">
+            <div className="to-do-container">
+                <h2>To Do List</h2>
+                <ul>
+                    {todos.map(({ completed, txt }, index) => (
+                        <div className="item">
+                            <li key={index}
+                                className={completed ? "done" : ""}
+                                onClick={() => { handleItemDone(index) }}
+                            >
+                                {txt}
+                            </li>
+                            <span onClick={() => { handeDeleteItem(index) }}>❌</span>
+                        </div>
+                    ))}
+                </ul>
+                <input ref={inputRef} placeholder="enter your todo" /><br />
+                <button onClick={handleAddTodo}>Add</button>
+            </div>
+        </div >
+    );
 }
 
 export default App;
